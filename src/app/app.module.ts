fd79ac4de1429c22a6fdf19d52b452fb394295db
Thomas Nix <thomasjnix.dev@gmail.com>
2020-12-07 19:46:28 -0800

    Stylistic updates, ready to incorproate basic API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageHeaderComponent } from './page-header/page-header.component';
-import { PageFooterComponent } from './page-footer/page-footer.component';
 import { AboutMeComponent } from './page-section/about-me/about-me.component';
 import { ProfessionalExperienceComponent } from './page-section/professional-experience/professional-experience.component';
 import { ContactAndResumeComponent } from './page-section/contact-and-resume/contact-and-resume.component';
@@ -15,7 +14,6 @@ import { ContactEmailService } from './page-section/contact-and-resume/contact-f
   declarations: [
     AppComponent,
     PageHeaderComponent,
-    PageFooterComponent,
     AboutMeComponent,
     ProfessionalExperienceComponent,
     ContactAndResumeComponent,
